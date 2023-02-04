36afa6d0ded801adc247ad8160e37aaa502f39f2
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-04 16:55:58 -0300

    feat: :fire: Remove código desnecessário

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { App } from "./Pages/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
