c4465483e22646b499d4a97a28fc2ff6cb42aee1
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-04 21:44:16 -0300

    build: :green_heart: Ajusta vite.config.ts
    Adiciona o abse para o deploy correto no gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        open: true,
-    },
+	plugins: [react()],
+	server: {
+		open: true,
+	},
+	base: "/react-calculator/",
 });
