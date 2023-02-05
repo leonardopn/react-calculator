cebd6f464b446cc67f45afa123f4fcfcbad03e1f
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-04 21:10:59 -0300

    feat: :lipstick: Cria novas cores no tema
    numberButton e operationButton

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -7,6 +7,19 @@ const global = {
 	},
 };
 
-const theme = extendTheme({ styles: { global } });
+const colors = {
+	numberButton: {
+		500: "#ffffff",
+		600: "#f3f3f3",
+		700: "#e6e6e6",
+	},
+	operationButton: {
+		500: "#FBB6CE",
+		600: "#F687B3",
+		700: "#F35C8D",
+	},
+};
+
+const theme = extendTheme({ styles: { global }, colors });
 
 export default theme;
