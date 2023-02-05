7a219ea638dcb3f74b1cda19ed76b157052cd5e3
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-02-04 21:26:16 -0300

    feat: :sparkles: Torna o CalculatorScreen em um input

--- src/components/Calculator/CalculatorScreen/index.tsx
+++ src/components/Calculator/CalculatorScreen/index.tsx
@@ -1,4 +1,5 @@
-import { Container } from "@chakra-ui/layout";
+import { Input } from "@chakra-ui/input";
+import { Container, Text } from "@chakra-ui/layout";
 
 interface CalculatorScreenProps {}
 
@@ -6,5 +7,9 @@ const CalculatorShadow =
 	" rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset";
 
 export function CalculatorScreen({}: CalculatorScreenProps) {
-	return <Container bg="white" w="full" h={20} boxShadow={CalculatorShadow}></Container>;
+	return (
+		<Container bg="white" w="full" h={20} boxShadow={CalculatorShadow} p={4} display="flex">
+			<Input size="lg" variant="unstyled" mt="auto" fontWeight="bold"></Input>
+		</Container>
+	);
 }
